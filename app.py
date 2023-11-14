c21581cf91dd437102c1044f6180f3468ddf3da8
Madhav Achar <acharmd@gmail.com>
2023-11-14 17:09:25 -0500

    Added function to get car info from FuelEconomy.gov

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+import requests
 
 
 class CreditScore(Enum):
@@ -68,4 +69,47 @@ def loan_payment(loan: Loan, car: Car, costs: Cost):
     total_interest = (loan.interest_rate / 12) * loan_amount * loan.term_length
     total_payments = total_interest + loan_amount
     costs.loan_payment = total_payments / loan.term_length
+
+
+def get_car_info(car_id: int) -> (str, float):
+    """
+    Get car information from FuelEconomy.gov API
+
+    Returns:
+        make: str
+        mpg: float
+    """
+    url = f"https://www.fueleconomy.gov/ws/rest/vehicle/{car_id}"
+    headers = {
+        "User-Agent": "CarLoanCalculator/1.0",
+        "Accept": "application/json"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+        
+        make = response_json["make"]
+        mpg = response_json["comb08"]
+    except requests.exceptions.RequestException as e:
+        print("Error requesting vehicle information for ID: ", car_id)
+        print(e)
+        
+        return None, None
+
+    return make, mpg
+
+########
+# Main #
+########
+
+car_id = input("Enter car ID: ")
+
+make, mpg = get_car_info(car_id)
+
+print("Car with ID: ", car_id)
+print("Make: ", make)
+print("MPG: ", mpg)
+
+# car = Car(make=make, cost=cost, mpg=mpg, cost=Cost(fuel=0, loan_payment=0, maintenance=0, total=0), fe_id=fe_id)
 
